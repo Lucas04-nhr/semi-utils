86c68fa8b4e57f3790463adf7beb2791c605268b
Lucas <lucas04@foxmail.com>
2024-05-03 22:24:04 +0800

    Revert

---
+++ image_container.py
@@ -0,0 +1,119 @@
+from datetime import datetime
+
+from PIL import Image
+from PIL.Image import Transpose
+
+from utils import get_exif
+
+
+class ImageContainer(object):
+    def __init__(self, path):
+        self.img = Image.open(path)
+        self.exif = get_exif(self.img)
+
+        # 相机机型
+        self.model = self.exif['Model'] if 'Model' in self.exif else '无'
+        # 相机制造商
+        self.make = self.exif['Make'] if 'Make' in self.exif else '无'
+        # 镜头型号
+        self.lens_model = self.exif['LensModel'] if 'LensModel' in self.exif else '无'
+        # 拍摄日期
+        self.date = self.exif['DateTimeOriginal'] \
+            if 'DateTimeOriginal' in self.exif \
+            else datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # 焦距
+        self.focal_length = int(self.exif['FocalLength']) if 'FocalLength' in self.exif else 0
+        # 等效焦距
+        self.focal_length_in_35mm_film = int(self.exif['FocalLengthIn35mmFilm']) \
+            if 'FocalLengthIn35mmFilm' in self.exif else self.focal_length
+        # 是否使用等效焦距
+        self.use_equivalent_focal_length = False
+        # 光圈大小
+        self.f_number = float(self.exif['FNumber']) if 'FNumber' in self.exif else .0
+        # 曝光时间
+        self.exposure_time = str(self.exif['ExposureTime'].real) \
+            if 'ExposureTime' in self.exif else '0s'
+        # 感光度
+        self.iso = self.exif['ISOSpeedRatings'] if 'ISOSpeedRatings' in self.exif else 0
+
+        # 修正图像方向
+        self.orientation = self.exif['Orientation'] if 'Orientation' in self.exif else 0
+        if self.orientation == 3:
+            self.img = self.img.transpose(Transpose.ROTATE_180)
+        elif self.orientation == 6:
+            self.img = self.img.transpose(Transpose.ROTATE_270)
+        elif self.orientation == 8:
+            self.img = self.img.transpose(Transpose.ROTATE_90)
+
+        # 水印设置
+        self.custom = '无'
+        self.logo = None
+
+        # 图像信息
+        self.original_width = self.exif['ExifImageWidth'] if 'ExifImageWidth' in self.exif else 0
+        self.original_length = self.exif['ExifImageHeight'] if 'ExifImageHeight' in self.exif else 0
+        self.width = self.img.width
+        self.height = self.img.height
+        self.ratio = self.width / self.height
+
+        # 水印图片
+        self.watermark_img = None
+
+    def _parse_datetime(self) -> str:
+        """
+        解析日期，转换为指定的格式
+        :return: 指定格式的日期字符串，转换失败返回原始的时间字符串
+        """
+        try:
+            date = datetime.strptime(self.date, '%Y:%m:%d %H:%M:%S')
+            return datetime.strftime(date, '%Y-%m-%d %H:%M')
+        except ValueError:
+            return self.date
+
+    def get_attribute_str(self, element) -> str:
+        """
+        通过 element 获取属性值
+        :param element: element 对象有 name 和 value 两个字段，通过 name 和 value 获取属性值
+        :return: 属性值字符串
+        """
+        if element is None or element.name == '':
+            return ''
+        if element.name == 'Model':
+            return self.model
+        elif element.name == 'Param':
+            return self.get_param_str()
+        elif element.name == 'Make':
+            return self.make
+        elif element.name == 'Date':
+            return self._parse_datetime()
+        elif element.name == 'LensModel':
+            return self.lens_model
+        elif element.name == 'Custom':
+            self.custom = element.value
+            return self.custom
+        else:
+            return ''
+
+    def get_param_str(self) -> str:
+        """
+        组合拍摄参数，输出一个字符串
+        :return: 拍摄参数字符串
+        """
+        focal_length = self.focal_length_in_35mm_film if self.use_equivalent_focal_length else self.focal_length
+        return '  '.join([str(focal_length) + 'mm', 'f/' + str(self.f_number), self.exposure_time,
+                          'ISO' + str(self.iso)])
+
+    def get_original_ratio(self):
+        return self.original_width / self.original_length
+
+    def get_logo(self):
+        return self.logo
+
+    def set_logo(self, logo) -> None:
+        self.logo = logo
+
+    def is_use_equivalent_focal_length(self, flag: bool) -> None:
+        self.use_equivalent_focal_length = flag
+
+    def close(self):
+        self.img.close()
--- main.py
+++ main.py
@@ -1,170 +1,325 @@
-import logging
-import sys
-from multiprocessing.pool import Pool
-from pathlib import Path
+import os
 
 from tqdm import tqdm
 
-from entity.image_container import ImageContainer
-from entity.image_processor import ProcessorChain
-from enums.constant import DEBUG
-from init import MARGIN_PROCESSOR
-from init import PADDING_TO_ORIGINAL_RATIO_PROCESSOR
-from init import SEPARATE_LINE
-from init import SHADOW_PROCESSOR
-from init import SIMPLE_PROCESSOR
-from init import config
-from init import layout_items_dict
-from init import root_menu
-from utils import ENCODING
-from utils import get_file_list
+from config import Layout, input_dir, config, font, bold_font, get_logo, white_margin_width, \
+    output_dir, quality, logo_enable, save_config, load_config
+from image_container import ImageContainer
+from image_processor import ImageProcessor, padding_image
+from utils import get_file_list, copy_exif_data
+
+id_to_name = {'Model': '相机机型', 'Make': '相机厂商', 'LensModel': '镜头型号', 'Param': '拍摄参数', 'Date': '拍摄时间',
+              'None': '无'}
+s_line = '+' + '-' * 15 + '+' + '-' * 15 + '+'
+id_to_loc = {'left_top': '左上文字', 'right_top': '右上文字', 'left_bottom': '左下文字', 'right_bottom': '右下文字'}
 
 
-def image_process_callback(processor_chain_, source_path_):
-    # 打开图片
-    container = ImageContainer(source_path_)
-
-    # 使用等效焦距
-    container.is_use_equivalent_focal_length(config.use_equivalent_focal_length())
-
-    # 处理图片
-    try:
-        processor_chain_.process(container)
-    except Exception as e:
-        logging.exception(f'Error: {str(e)}')
-        if DEBUG:
-            raise e
-        else:
-            print(f'\nError: 文件：{source_path_} 处理失败，请检查日志')
-
-    # 保存图片
-    target_path = Path(config.get_output_dir(), encoding=ENCODING).joinpath(source_path_.name)
-
-    container.save(target_path, quality=config.get_quality())
-    container.close()
+def parse_elem_value(element):
+    if element['name'] == 'Custom':
+        return '自定义字段 (' + (element['value'] if 'value' in element else '') + ')'
+    else:
+        return id_to_name.setdefault(element['name'], '值错误')
 
 
-def processing():
+def print_current_setting():
     """
-    状态100：处理图片
+    状态1：开始菜单
     :return:
     """
     global state
 
-    file_list = get_file_list(config.get_input_dir())
+    print(s_line)
+    print(' ' * 8 + '当前设置')
+    print(s_line)
+    print(' 【1】: 布局：{}'.format(config['layout']['type']))
+    print(' 【2】: Logo：{}'.format("显示" if config['logo']['enable'] else "不显示"))
+    print(' 【3】: 左上文字：{}'.format(parse_elem_value(config['layout']['elements']['left_top'])))
+    print(' 【4】: 左下文字：{}'.format(parse_elem_value(config['layout']['elements']['left_bottom'])))
+    print(' 【5】: 右上文字：{}'.format(parse_elem_value(config['layout']['elements']['right_top'])))
+    print(' 【6】: 右下文字：{}'.format(parse_elem_value(config['layout']['elements']['right_bottom'])))
+    print(' 【7】: 白色边框：{}'.format("显示" if config['layout']['white_margin']['enable'] else "不显示"))
+    print(' 【8】: 等效焦距：{}'.format("使用" if config['param']['focal_length']['use_equivalent_focal_length'] else "不使用"))
+    print(s_line)
+    user_input = input('输入【y 或回车】按照当前设置开始处理图片，输入【数字】修改设置，输入【x】退出程序\n')
+    if user_input == 'y' or user_input == '':
+        state = 100
+    elif user_input == 'x' or user_input == 'X':
+        exit(0)
+    elif user_input.isdigit():
+        state = int(user_input)
+
+
+def processing():
+    """
+    状态1：处理图片
+    :return:
+    """
+    global state
+
+    file_list = get_file_list(input_dir)
+    layout = Layout(config['layout'])
     print('当前共有 {} 张图片待处理'.format(len(file_list)))
+    processor = ImageProcessor(font, bold_font)
+    for file in tqdm(file_list):
+        source_path = os.path.join(input_dir, file)
+        # 打开图片
+        container = ImageContainer(source_path)
 
-    processor_chain = ProcessorChain()
-
-    # 如果需要添加阴影，则添加阴影处理器，阴影处理器优先级最高，但是正方形布局不需要阴影
-    if config.has_shadow_enabled() and 'square' != config.get_layout_type():
-        processor_chain.add(SHADOW_PROCESSOR)
-
-    # 根据布局添加不同的水印处理器
-    if config.get_layout_type() in layout_items_dict:
-        processor_chain.add(layout_items_dict.get(config.get_layout_type()).processor)
-    else:
-        processor_chain.add(SIMPLE_PROCESSOR)
-
-    # 如果需要添加白边，且是水印布局，则添加白边处理器，白边处理器优先级最低
-    if config.has_white_margin_enabled() and 'watermark' in config.get_layout_type():
-        processor_chain.add(MARGIN_PROCESSOR)
-
-    # 如果需要按原有比例填充，且不是正方形布局，则添加填充处理器
-    if config.has_padding_with_original_ratio_enabled() and 'square' != config.get_layout_type():
-        processor_chain.add(PADDING_TO_ORIGINAL_RATIO_PROCESSOR)
-
-    def update(*a):
-        # 这个函数将会在每个进程完成后被调用，用来更新进度条
-        pbar.update()
-
-    # 初始化tqdm进度条
-    pbar = tqdm(total=len(file_list))
-
-    # 限制最大并发进程数
-    pool = Pool(5)
-
-    # 对于file_list中的每个source_path，异步地启动进程
-    for source_path in file_list:
-        pool.apply_async(image_process_callback, args=(processor_chain, source_path), callback=update)
-
-    # 关闭进程池，不再接受新的任务
-    pool.close()
-
-    # 等待所有进程完成
-    pool.join()
-
-    # 完成所有任务后，关闭tqdm进度条
-    pbar.close()
-
-    option = input('处理完成，文件已输出至 output 文件夹中，输入【r】返回主菜单，输入【x】退出程序\n')
-    if DEBUG:
-        sys.exit(0)
-    else:
-        if option == 'x' or option == 'X':
-            state = -1
-            # r 返回上一层
+        # 添加logo
+        if logo_enable:
+            container.set_logo(get_logo(container.make))
         else:
-            state = 0
+            container.set_logo(None)
+
+        # 使用等效焦距
+        container.is_use_equivalent_focal_length(config['param']['focal_length']['use_equivalent_focal_length'])
+
+        # 添加水印
+        if 'normal_with_right_logo' == layout.type:
+            watermark = processor.normal_watermark(container, layout, is_logo_left=False)
+        elif 'square' == layout.type:
+            watermark = processor.square_watermark(container)
+        else:
+            watermark = processor.normal_watermark(container, layout)
+
+        # 添加白框
+        if config['layout']['white_margin']['enable']:
+            watermark = padding_image(watermark, int(white_margin_width * min(container.width, container.height) / 100),
+                                      'tlr')
+
+        # 保存图片
+        target_path = os.path.join(output_dir, file)
+        watermark.save(target_path, quality=quality)
+        container.close()
+        watermark.close()
+        copy_exif_data(source_path, target_path)
+    print('处理完成，文件已输出至 output 文件夹中，请点击任意键退出或直接关闭'.format(len(file_list)))
+    input()
+    state = -1
+
+def modify_focal_length():
+    global state
+
+    while True:
+        print('输入【数字】选择是否使用等效焦距：')
+        print('    【1】: 使用等效焦距')
+        print('    【2】: 不使用等效焦距')
+        print('输入【0】返回主菜单')
+        print('输入【x】退出程序')
+        user_input = input()
+        if user_input == 'x' or user_input == 'X':
+            exit(0)
+        if user_input.isdigit():
+            if user_input == '0':
+                return
+            elif user_input == '1':
+                config['param']['focal_length']['use_equivalent_focal_length'] = True
+                return
+            elif user_input == '2':
+                config['param']['focal_length']['use_equivalent_focal_length'] = False
+                return
+            else:
+                print('输入错误，请重新输入')
+        else:
+            print('输入错误，请重新输入')
+
+def modify_layout():
+    """
+    状态2：修改布局
+    :return:
+    """
+    global state
+
+    while True:
+        print('输入【数字】选择布局类型：')
+        print('    【1】: normal')
+        print('    【2】: normal_with_right_logo')
+        print('    【3】: square')
+        print('输入【0】返回主菜单')
+        print('输入【x】退出程序')
+        user_input = input()
+        if user_input == 'x' or user_input == 'X':
+            exit(0)
+        if user_input.isdigit():
+            if user_input == '0':
+                return
+            elif user_input == '1':
+                config['layout']['type'] = 'normal'
+                return
+            elif user_input == '2':
+                config['layout']['type'] = 'normal_with_right_logo'
+                return
+            elif user_input == '3':
+                config['layout']['type'] = 'square'
+                return
+            else:
+                print('输入错误，请重新输入')
+        else:
+            print('输入错误，请重新输入')
+
+
+def modify_logo():
+    """
+    状态3：修改 logo
+    :return:
+    """
+    global state
+
+    while True:
+        print('输入【数字】选择是否显示 logo：')
+        print('    【1】: 显示 logo')
+        print('    【2】: 不显示 logo')
+        print('输入【0】返回主菜单')
+        print('输入【x】退出程序')
+        user_input = input()
+        if user_input == 'x' or user_input == 'X':
+            exit(0)
+        if user_input.isdigit():
+            if user_input == '0':
+                return
+            elif user_input == '1':
+                config['logo']['enable'] = True
+                return
+            elif user_input == '2':
+                config['logo']['enable'] = False
+                return
+            else:
+                print('输入错误，请重新输入')
+        else:
+            print('输入错误，请重新输入')
+
+
+def modify_white_margin():
+    """
+    状态4：修改 margin
+    :return:
+    """
+    global state
+
+    while True:
+        print('输入【数字】选择是否显示白色边框：')
+        print('    【1】: 显示白色边框')
+        print('    【2】: 不显示白色边框')
+        print('输入【0】返回主菜单')
+        print('输入【x】退出程序')
+        user_input = input()
+        if user_input == 'x' or user_input == 'X':
+            exit(0)
+        if user_input.isdigit():
+            if user_input == '0':
+                return
+            elif user_input == '1':
+                config['layout']['white_margin']['enable'] = True
+                return
+            elif user_input == '2':
+                config['layout']['white_margin']['enable'] = False
+                return
+            else:
+                print('输入错误，请重新输入')
+        else:
+            print('输入错误，请重新输入')
+
+
+def modify_element(key):
+    """
+    状态4-7：修改 logo
+    :return:
+    """
+    global state
+
+    while True:
+        print('输入【数字】选择【{}】显示内容：'.format(id_to_loc[key]))
+        print('    【1】：相机型号')
+        print('    【2】：相机厂商')
+        print('    【3】：镜头型号')
+        print('    【4】：拍摄参数')
+        print('    【5】：拍摄日期')
+        print('    【6】：自定义字段')
+        print('    【7】：无')
+        print('输入【0】返回主菜单')
+        print('输入【x】退出程序')
+        user_input = input()
+        if user_input == 'x' or user_input == 'X':
+            exit(0)
+        if user_input.isdigit():
+            number = int(user_input)
+            if number == 0:
+                return
+            elif number == 1:
+                config['layout']['elements'][key]['name'] = 'Model'
+                return
+            elif number == 2:
+                config['layout']['elements'][key]['name'] = 'Make'
+                return
+            elif number == 3:
+                config['layout']['elements'][key]['name'] = 'LensModel'
+                return
+            elif number == 4:
+                config['layout']['elements'][key]['name'] = 'Param'
+                return
+            elif number == 5:
+                config['layout']['elements'][key]['name'] = 'Date'
+                return
+            elif number == 6:
+                config['layout']['elements'][key]['name'] = 'Custom'
+                user_input = input('输入自定义字段的值：\n')
+                config['layout']['elements'][key]['value'] = user_input
+                return
+            elif number == 7:
+                config['layout']['elements'][key]['name'] = 'None'
+                return
+            else:
+                print('输入错误，请重新输入')
+        else:
+            print('输入错误，请重新输入')
 
 
 state = 0
-current_menu = root_menu
-root_menu.set_parent(root_menu)
 
 if __name__ == '__main__':
-    print(SEPARATE_LINE)
-    print('''
-本工具为开源工具，遵循 Apache 2.0 License 发布。如果您在使用过程中遇到问题，请联系作者：
-GitHub: @leslievan
-Bilibili: @吨吨吨的半夏
-项目介绍：https://www.bilibili.com/video/BV11A411U7Kn
-项目地址：https://github.com/leslievan/semi-utils
-项目介绍（博客）：https://lsvm.xyz/2023/02/semi-utils-intro/
-项目发布页：https://docs.qq.com/sheet/DTXF5c2lHeUZYREtw
-''')
+
     while True:
-        try:
-            # 0：主菜单，100：处理图片，-1：退出程序，其他：子菜单
-            if state == 0:
-                # 显示主菜单
-                print(SEPARATE_LINE)
-                current_menu.display()
-                print(SEPARATE_LINE)
-
-                # 处理用户输入
-                user_input = input(
-                    '输入【y 或回车】按照当前设置开始处理图片，输入【数字】修改设置，输入【r】返回上一层菜单，输入【x】退出程序\n')
-
-                # y 或回车，跳转状态 100，开始处理图片
-                if user_input == 'y' or user_input == '':
-                    state = 100
-                # x 退出程序
-                elif user_input == 'x' or user_input == 'X':
-                    sys.exit(0)
-                # r 返回上一层
-                elif user_input == 'r' or user_input == 'R':
-                    current_menu = current_menu.get_parent()
-                # 数字合法则跳转到对应子菜单
-                elif user_input.isdigit() and 1 <= int(user_input) <= len(current_menu.components):
-                    current_menu = current_menu.components[int(user_input) - 1]
-                    if current_menu.is_leaf():
-                        current_menu.run()
-                        current_menu = root_menu
-                else:
-                    print('输入错误，请重新输入')
-            elif state == 100:
-                # 处理数据的代码
-                print(SEPARATE_LINE)
-                processing()
-            elif state == -1:
-                # 退出程序
-                sys.exit(0)
-            elif state == -2:
-                sys.exit(1)
-            # 保存配置
-            config.save()
-        except Exception as e:
-            logging.exception(f'Error: {str(e)}')
-            if DEBUG:
-                raise e
+        if state == 0:
+            print_current_setting()
+        elif state == 100:
+            # 处理数据的代码
+            print(s_line)
+            load_config()
+            processing()
+        elif state == 1:
+            modify_layout()
+            # 修改布局类型的代码
+            state = 0
+        elif state == 2:
+            modify_logo()
+            # 修改logo的代码
+            state = 0
+        elif state == 3:
+            modify_element('left_top')
+            # 修改左上文字的代码
+            state = 0
+        elif state == 4:
+            modify_element('left_bottom')
+            # 修改左下文字的代码
+            state = 0
+        elif state == 5:
+            modify_element('right_top')
+            # 修改右上文字的代码
+            state = 0
+        elif state == 6:
+            modify_element('right_bottom')
+            # 修改右下文字的代码
+            state = 0
+        elif state == 7:
+            modify_white_margin()
+            # 修改右下文字的代码
+            state = 0
+        elif state == 8:
+            modify_focal_length()
+            state = 0
+        elif state == -1:
+            exit(0)
+        else:
+            print("输入错误，请重新输入")
+            state = 0
+        save_config()
